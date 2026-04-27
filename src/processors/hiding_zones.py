8fb6ff4e2afe6173a3d45521f5920e2cca0dcdbf
BertMoors <bertmoors99@gmail.com>
2026-04-27 11:21:51 +0200

    reworked hiding zone processor to work with calculated crs values if none is provided

--- src/processors/hiding_zones.py
+++ src/processors/hiding_zones.py
@@ -1,32 +1,30 @@
-from geopandas import GeoSeries
 from geopandas.geodataframe import GeoDataFrame
+from pandas import concat
 from numpy import copysign, floor
 
+
+def calculate_epsg(row):
+    point = row["geometry"]
+    return int(32700 - (copysign(1, point.y) + 1) / 2 * 100 + (floor((180 + point.x) / 6) + 1))
 
 class HidingZones:
     def __init__(self, config):
         self.size = config.get('size',250)
         self.epsg = config.get('epsg',None)
 
-    def calculate_epsg(self, row):
-        if isinstance(row, GeoSeries):
-            point = row[0]
-        elif isinstance(row, GeoDataFrame):
-            point = row
-        else:
-            raise NotImplementedError
-        y,x =point.y, point.x
-        return int(32700 - (copysign(1, y ) + 1) / 2 * 100 + (floor((180 + x) / 6) + 1))
-
-
     def process(self, df):
         new_df = df.copy()
         new_df.set_crs(4326,inplace=True)
-        epsg = self.epsg
-        if not epsg:
-            epsg = self.calculate_epsg(new_df.centroid)
-        new_df.to_crs(epsg, inplace=True)
+        if not self.epsg:
+            new_df["epsg"] = new_df.apply(calculate_epsg, axis=1)
 
-        new_df['geometry'] = new_df['geometry'].buffer(distance=self.size)
-        new_df.to_crs(4326,inplace=True)
-        return new_df
+        # go through all unique epsg values and draw hiding zones
+        partials = []
+        for epsg in new_df["epsg"].unique():
+            partial_df = new_df[new_df["epsg"] == epsg]
+            partial_df.to_crs(epsg, inplace=True)
+            partial_df['geometry'] = partial_df['geometry'].buffer(distance=self.size)
+            partial_df.to_crs(4326, inplace=True)
+            partials.append(partial_df)
+        # re-combine all partial data frames into one
+        return GeoDataFrame(concat(partials), crs="EPSG:4326")
