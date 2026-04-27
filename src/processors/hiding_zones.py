d50be1cf8f09f8a2640d179d616a14341d487280
Nander Voortman <nander@nander.org>
2026-04-27 11:03:39 +0200

    Add auto-epsg

--- src/processors/hiding_zones.py
+++ src/processors/hiding_zones.py
@@ -1,12 +1,31 @@
+from geopandas import GeoSeries
+from geopandas.geodataframe import GeoDataFrame
+from numpy import copysign, floor
+
+
 class HidingZones:
     def __init__(self, config):
-        self.size = config.get('size',.25)
-        self.epsg = config.get('epsg',32531)
+        self.size = config.get('size',250)
+        self.epsg = config.get('epsg',None)
+
+    def calculate_epsg(self, row):
+        if isinstance(row, GeoSeries):
+            point = row[0]
+        elif isinstance(row, GeoDataFrame):
+            point = row
+        else:
+            raise NotImplementedError
+        y,x =point.y, point.x
+        return int(32700 - (copysign(1, y ) + 1) / 2 * 100 + (floor((180 + x) / 6) + 1))
+
 
     def process(self, df):
         new_df = df.copy()
         new_df.set_crs(4326,inplace=True)
-        new_df.to_crs(self.epsg, inplace=True)
+        epsg = self.epsg
+        if not epsg:
+            epsg = self.calculate_epsg(new_df.centroid)
+        new_df.to_crs(epsg, inplace=True)
 
         new_df['geometry'] = new_df['geometry'].buffer(distance=self.size)
         new_df.to_crs(4326,inplace=True)
