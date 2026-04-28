c7b1e2d23d9a36c66b80fca25d3cca919c2a5b3b
BertMoors <bertmoors99@gmail.com>
2026-04-28 11:07:36 +0200

    Updated hiding zone processor. added variable to select if output is polygon or line. made default output line

--- src/processors/hiding_zones.py
+++ src/processors/hiding_zones.py
@@ -1,6 +1,7 @@
 from geopandas.geodataframe import GeoDataFrame
-from pandas import concat
 from numpy import copysign, floor
+from pandas import concat
+from shapely import MultiLineString
 
 
 def calculate_epsg(row):
@@ -9,8 +10,9 @@ def calculate_epsg(row):
 
 class HidingZones:
     def __init__(self, config):
-        self.size = config.get('size',250)
-        self.epsg = config.get('epsg',None)
+        self.size = config.get("size",250)
+        self.epsg = config.get("epsg",None)
+        self.draw_polygons = config.get("draw_polygons", False)
 
     def process(self, df):
         new_df = df.copy()
@@ -23,7 +25,12 @@ class HidingZones:
         for epsg in new_df["epsg"].unique():
             partial_df = new_df[new_df["epsg"] == epsg]
             partial_df.to_crs(epsg, inplace=True)
-            partial_df['geometry'] = partial_df['geometry'].buffer(distance=self.size)
+            if self.draw_polygons:
+                partial_df['geometry'] = partial_df['geometry'].buffer(distance=self.size)
+            else:
+                circles = partial_df.buffer(self.size, 12).boundary
+                circles = circles.apply(lambda x: MultiLineString([x.coords, x.coords[-2::]]))
+                partial_df['geometry'] = circles
             partial_df.to_crs(4326, inplace=True)
             partials.append(partial_df)
         # re-combine all partial data frames into one
