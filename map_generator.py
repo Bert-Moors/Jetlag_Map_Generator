a53b51a91e2f617212979739dbde3192118077d9
Nander Voortman <orville@nander.net>
2026-05-12 19:16:05 +0200

    Switch default format to toml.

--- map_generator.py
+++ map_generator.py
@@ -1,5 +1,14 @@
 import src.generator as gen
-
+import sys
 if __name__ == "__main__":
-    map_gen = gen.Generator("input/input.json", "output/brussels")
+    print(sys.argv)
+    path = "brussels.toml"
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+    ext = path.split(".")[-1]
+    if ext == "json":
+        map_gen = gen.Generator("input/"+path, "output/"+path.split(".")[0])
+    if ext == "toml":
+        map_gen = gen.Generator("input/"+path, "output/"+path.split(".")[0], typ="toml")
+
     map_gen.generate()
